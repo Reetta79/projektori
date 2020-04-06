8973f7e37b9ee8295a96f20792471da1ec487bc2
Reetta79 <reettasaar@gmail.com>
2020-04-06 17:34:32 +0300

    Kokeiluja ei kälissä

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import './Header.css'
 
+
+
 function Header(props) {
     return(
       <div className ="header">
         <h1>Projektori</h1>
+       
       </div>
     );
   }
---
+++ src/components/ShowHide/ShowHide.js
@@ -0,0 +1,24 @@
+const checkboxes = [
+    {
+      name: 'check-box-1',
+      key: 'checkBox1',
+      label: 'Valmis',
+    },
+    {
+      name: 'check-box-2',
+      key: 'checkBox2',
+      label: 'Uusi järjestelmä',
+    },
+    {
+      name: 'check-box-3',
+      key: 'checkBox3',
+      label: 'Järjestelmän jatkokehitys',
+    },
+    {
+      name: 'check-box-4',
+      key: 'checkBox4',
+      label: 'Prosessin kehitys',
+    },
+  ];
+  
+  export default checkboxes;
